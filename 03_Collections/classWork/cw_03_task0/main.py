190f389b05a3e95e2c3852e00286d621f5bfa7df
atrem.bezruk <artem.bezruk@gmail.com>
2023-04-22 22:12:11 +0300

    Seminar#02 class work task#0 corrected

--- 03_Collections/classWork/cw_03_task0/main.py
+++ 03_Collections/classWork/cw_03_task0/main.py
@@ -5,9 +5,8 @@ import random
 
 
 def main():
-    numbers = list()
-    for i in range(0, 10):
-        numbers.append(random.randint(0, 10))
+    listLength = random.randint(2, 10)
+    numbers = [random.randint(0, 10) for index in range(0, listLength)]
     print(numbers)
     sumInt = sum(numbers)
     print(str(sumInt) + " is " + ("even" if sumInt % 2 == 0 else "odd"))
