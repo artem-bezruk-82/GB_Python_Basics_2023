e971aeea90feeaa1bb299514c85d6c2caa2a53d9
atrem.bezruk <artem.bezruk@gmail.com>
2023-05-11 18:36:17 +0300

    Seminar#3 class work task#2 corected

--- 03_Collections/classWork/cw_03_task2/main.py
+++ 03_Collections/classWork/cw_03_task2/main.py
@@ -5,9 +5,25 @@
 
 
 def main():
+    print("*** variant #1 with tuple***")
     print("Hi, please fill in our short questionary form")
     questionnaireTuple = get_questionnaire_console_tuple()
     questionnaire_tuple_print_console(*questionnaireTuple)
+
+    print("*** variant #2 with dictionary ***")
+    print("Hi, please fill in our short questionary form")
+    questionnaireDic = get_questionnaire_console_dict()
+    for key in questionnaireDic:
+        print(f"{key}: {questionnaireDic[key]}", end=' ')
+
+
+def get_questionnaire_console_dict():
+    questionnaireDict = dict()
+    questionnaireDict["name"] = str(input("Please enter your name: "))
+    questionnaireDict["age"] = abs(get_console_input_int("Please enter your age: "))
+    questionnaireDict["hobby"] = str(input("Please enter your hobby: "))
+    questionnaireDict["animal"] = str(input("Please enter your favourite animal: "))
+    return questionnaireDict
 
 
 def get_questionnaire_console_tuple():
