2b9058d9156fe646283bd0cc47d15bd126e02db6
atrem.bezruk <artem.bezruk@gmail.com>
2023-05-21 14:26:23 +0300

    Seminar#5 class work task#1 corrected

--- 05_Lambda/classWork/cw_05_task1/main.py
+++ 05_Lambda/classWork/cw_05_task1/main.py
@@ -6,11 +6,22 @@ import random
 def main():
     valueInt = random.randint(1000, 10000)
     print(valueInt)
-    valuesList = list()
-    while valueInt > 1:
-        valuesList.append((valueInt % 10)+10)
+    print("variant 1")
+    digitsList1 = get_digits_list(valueInt, 10)
+    print(digitsList1)
+    print("variant 2")
+    digitsList2 = [int(digit) + 10 for digit in str(valueInt)]
+    print(digitsList2)
+
+
+def get_digits_list(valueInt, increaseInt = 0):
+    valueInt = abs(int(valueInt))
+    digits = list()
+    while valueInt >= 1:
+        digits.append(valueInt % 10)
         valueInt = int(valueInt / 10)
-    print(valuesList)
+    digits = list(map(lambda digit: digit + abs(int(increaseInt)), digits))
+    return list(reversed(digits))
 
 
 main()
