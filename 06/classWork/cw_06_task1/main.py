0a40e929ad1fbac8e904041c94f4ce88c701c30d
atrem.bezruk <artem.bezruk@gmail.com>
2023-05-24 21:33:43 +0300

    Seminar#6 class work task#1 done

---
+++ 06/classWork/cw_06_task1/main.py
@@ -0,0 +1,15 @@
+# Семинар 5
+# Задача 1
+# Дан список случайных элементов. Проверьте, что все его элементы уникальны.
+import random
+
+
+def main():
+    randomNumbersList = [random.randint(0, 9) for _ in range(0, random.randint(3, 15))]
+    print(randomNumbersList)
+    print(set(randomNumbersList))
+    print("List elements are" + (" " if len(randomNumbersList) == len(set(randomNumbersList)) else " not ") + "unique")
+
+
+main()
+
