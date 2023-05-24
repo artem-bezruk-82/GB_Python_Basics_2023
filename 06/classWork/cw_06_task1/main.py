cc6bdd669c6d8bc746413c048ad1b7e60da5d8ec
atrem.bezruk <artem.bezruk@gmail.com>
2023-05-24 21:34:15 +0300

    Seminar#6 class work task#1 corrected

--- 06/classWork/cw_06_task1/main.py
+++ 06/classWork/cw_06_task1/main.py
@@ -7,7 +7,6 @@ import random
 def main():
     randomNumbersList = [random.randint(0, 9) for _ in range(0, random.randint(3, 15))]
     print(randomNumbersList)
-    print(set(randomNumbersList))
     print("List elements are" + (" " if len(randomNumbersList) == len(set(randomNumbersList)) else " not ") + "unique")
 
 
